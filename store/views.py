a1068218cac1c89b33fd0176ac5a585c35aa744b
Zsolt Nagy <info@zsoltnagy.eu>
2023-09-09 01:43:33 +0200

    bootstrap navbar

--- store/views.py
+++ store/views.py
@@ -5,7 +5,8 @@ from .models import Book
 # Create your views here.
 def index(request):
     context = {
-        'page': 'Mystery Books'
+        'page': 'Mystery Books',
+        'menu_item': 'index'
     }
     return render(request, 'store/template.html', context)
 
@@ -14,7 +15,8 @@ def store(request):
    count = Book.objects.all().count()
    context = {
        'count': count,
-       'page': 'Welcome to Mystery Books'
+       'page': 'Welcome to Mystery Books',
+       'menu_item': 'store'
    }
    return render(request, "store/store.html", context)
 
