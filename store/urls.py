27efd87461304fb2b19d2aff5c89c5659661155c
Zsolt Nagy <info@zsoltnagy.eu>
2023-09-19 01:42:22 +0200

    add feature

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('store/', views.store, name='store'),
     path('store/<int:book_id>', views.display_book, name='display_book'),
+    path('store/new', views.new_book, name='new_book'),
 ]
--- store/views.py
+++ store/views.py
@@ -12,15 +12,16 @@ def index(request):
 
 
 def store(request):
-   books = Book.objects.all()
-   count = books.count()
-   context = {
-       'count': count,
-       'books': books,
-       'page': 'Welcome to Mystery Books',
-       'menu_item': 'store'
-   }
-   return render(request, "store/store.html", context)
+    books = Book.objects.all()
+    count = books.count()
+    context = {
+        'count': count,
+        'books': books,
+        'page': 'Welcome to Mystery Books',
+        'menu_item': 'store'
+    }
+    return render(request, "store/store.html", context)
+
 
 def display_book(request, book_id):
     try:
@@ -35,3 +36,27 @@ def display_book(request, book_id):
         'book': book
     }
     return render(request, 'store/book.html', context) # TODO: write new template
+
+
+def new_book(request):
+    context = {
+        'page': 'New Book',
+        'menu_item': 'store',
+        'create_successful': False,
+        'post_error': False,
+    }
+    if request.method == 'POST':
+        try:
+            new_book = Book(
+                title = request.POST.get('title'),
+                author = request.POST.get('author'),
+                description = request.POST.get('description'),
+                publish_date = request.POST.get('date_of_pub'),
+                price = request.POST.get('price'),
+                stock = request.POST.get('stock')
+            )
+            new_book.save()
+            context['create_successful'] = True
+        except:
+            context['post_error'] = True
+    return render(request, 'store/new_book.html', context)
